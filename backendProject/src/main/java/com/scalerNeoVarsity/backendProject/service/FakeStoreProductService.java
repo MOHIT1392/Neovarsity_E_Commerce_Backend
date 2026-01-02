fef00351f3c591fe54ff041061f9d4a5c18498f4
Mohit Gupta <antriksh.mbm@gmail.com>
2026-01-02 13:52:56 +0530

    adding delete and update functionality in fakestore product service

--- backendProject/src/main/java/com/scalerNeoVarsity/backendProject/service/FakeStoreProductService.java
+++ backendProject/src/main/java/com/scalerNeoVarsity/backendProject/service/FakeStoreProductService.java
@@ -45,13 +45,44 @@ public class FakeStoreProductService implements ProductService {
 
     @Override
     public Product deleteProduct(Long id) {
-        return null;
+        Product deletedProduct = getSingleProduct(id);
+        restTemplate.delete("https://fakestoreapi.com/products/" + id);
+        return deletedProduct;
     }
 
     @Override
     public Product updateProduct(Long id, String title, String description, Double price, Category category, String imageUrl) {
-        return null;
+        System.out.println("Inside the update product in FakeStoreProductService API");
+        Product existingProduct = getSingleProduct(id);
+
+        if (existingProduct != null) {
+            FakeStoreProductDTO fakeStoreProductDTO = new FakeStoreProductDTO();
+            System.out.println("Updating the Product");
+            fakeStoreProductDTO.setId(id);
+            if (title != null) {
+                existingProduct.setTitle(title);
+            }
+            if (description != null) {
+                existingProduct.setDescription(description);
+            }
+            if (price != null) {
+                existingProduct.setPrice(price);
+            }
+            if (category != null) {
+                existingProduct.setCategory(category);
+            }
+            if (imageUrl != null) {
+                existingProduct.setImageUrl(imageUrl);
+            }
+            Product response =
+                    restTemplate.patchForObject("https://fakestoreapi.com/products/" + id,
+                            existingProduct, Product.class);
+            return response;
+        } else {
+            throw new RuntimeException("Product Not Exist");
+        }
     }
+
     @Override
     public List<Product> getAllProducts() {
         System.out.println("In the getAllProducts API in FKSPS");
