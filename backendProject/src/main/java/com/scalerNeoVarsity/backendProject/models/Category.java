422e4abfc7c4e5f2508225a925de99e9ab26aff2
Mohit Gupta <antriksh.mbm@gmail.com>
2026-01-10 14:38:52 +0530

    adding cardinality for category

--- backendProject/src/main/java/com/scalerNeoVarsity/backendProject/models/Category.java
+++ backendProject/src/main/java/com/scalerNeoVarsity/backendProject/models/Category.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+
 import java.util.List;
 
 @Getter
@@ -26,15 +29,34 @@ public class Category extends BaseModel {
 
     @JsonIgnore
     private List<Product> products;
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+    private List<Product> products;
 
-    public Category(String testCategory) {
+    public String getTitle() {
+        return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 
     @Override
     public String toString() {
         return "Category{" +
                 "title='" + title + '\'' +
+                ", products=" + products +
                 '}';
     }
+
+    public Category(String testCategory) {
+    }
+
 }
