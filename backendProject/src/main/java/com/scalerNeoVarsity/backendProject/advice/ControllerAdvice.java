399034aa91528200383df360c7910fc901e736bb
Mohit Gupta <antriksh.mbm@gmail.com>
2026-03-29 20:41:54 +0530

    adding controller advice

---
+++ backendProject/src/main/java/com/scalerNeoVarsity/backendProject/advice/ControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.scalerNeoVarsity.backendProject.advice;
+
+
+
+import com.scalerNeoVarsity.backendProject.dto.ErrorDTO;
+import com.scalerNeoVarsity.backendProject.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleProductNotFoundException(ProductNotFoundException ex) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setMessage(ex.getMessage());
+
+        //Local variable 'errorResponseEntity' is redundant
+        return new ResponseEntity<>(
+                errorDTO,
+                HttpStatus.NOT_FOUND
+        );
+    }
+}
